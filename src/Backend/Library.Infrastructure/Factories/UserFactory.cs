1bbeacbf5df11dcf4f7872d305dcd12b574d1059
Karol <karol_maliglowka@o2.pl>
2025-05-18 15:29:19 +0200

    added missing file

---
+++ src/Backend/Library.Infrastructure/Factories/UserFactory.cs
@@ -0,0 +1,42 @@
+﻿using Library.Core.Entities;
+using Library.Core.ValueObjects;
+using Library.Infrastructure.DTO;
+
+namespace Library.Infrastructure.Factories;
+
+public static class UserFactory
+{
+    public static User CreateUser(UserDto userDto)
+    {
+        ArgumentNullException.ThrowIfNull(userDto);
+
+        return new User.Builder()
+            .SetName(new Name(userDto.Name))
+            .SetSurname(userDto.Surname)
+            .SetEmail(userDto.Email)
+            .SetAddress(userDto.Address)
+            .SetPhoneNumber(userDto.PhoneNumber)
+            .SetCity(userDto.City)
+            .SetCountry(userDto.Country)
+            .SetPostalCode(userDto.PostalCode)
+            .SetActive(true)
+            .Build();
+    }
+    
+    public static User EditUser(UserDto userDto, User currentUser)
+    {
+        ArgumentNullException.ThrowIfNull(userDto);
+
+        return new User.Builder(currentUser)
+            .SetName(new Name(userDto.Name))
+            .SetSurname(userDto.Surname)
+            .SetEmail(userDto.Email)
+            .SetAddress(userDto.Address)
+            .SetPhoneNumber(userDto.PhoneNumber)
+            .SetCity(userDto.City)
+            .SetCountry(userDto.Country)
+            .SetPostalCode(userDto.PostalCode)
+            .SetActive(true)
+            .Build();
+    }
+}
