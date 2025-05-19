960e49304c69e88eec298944910fbf48fed5d190
Karol <karol_maliglowka@o2.pl>
2025-05-19 21:41:10 +0200

    changes

---
+++ src/Backend/Library.Infrastructure/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using Library.Core.Entities;
+using Library.Infrastructure.DTO;
+
+namespace Library.Infrastructure;
+
+public static class Extensions
+{
+    public static UserDto BuildUserDto(this User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Surname = user.Surname,
+            Email = user.Email,
+            Address = user.Address,
+            PhoneNumber = user.PhoneNumber,
+            City = user.City,
+            Country = user.Country,
+            PostalCode = user.PostalCode,
+            IsActive = user.IsActive,
+            FullName = user.FullName
+        };
+    }
+}
---
+++ src/Backend/Library.Infrastructure/Factories/PublisherFactory.cs
@@ -0,0 +1,24 @@
+﻿using Library.Core.Entities;
+using Library.Infrastructure.DTO;
+
+namespace Library.Infrastructure.Factories;
+
+public static class PublisherFactory
+{
+
+    public static Publisher CreatePublisher(PublisherDto publisherDto)
+    {
+        ArgumentNullException.ThrowIfNull(publisherDto);
+        return new Publisher.Builder()
+            .SetName(publisherDto.Name)
+            .Build();
+    }
+    
+    public static Publisher EditPublisher(PublisherDto publisherDto, Publisher publisher)
+    {
+        ArgumentNullException.ThrowIfNull(publisherDto);
+        return new Publisher.Builder(publisher)
+            .SetName(publisherDto.Name)
+            .Build();
+    }
+}
