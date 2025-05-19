1ec979e020afdfc6be25b48564da02f7380bae6a
Karol <karol_maliglowka@o2.pl>
2025-05-19 14:11:24 +0200

    changes

--- src/Backend/Library.Core/Entities/Publisher.cs
+++ src/Backend/Library.Core/Entities/Publisher.cs
@@ -3,7 +3,7 @@
 namespace Library.Core.Entities;
 public sealed class Publisher : BaseClass
 {
-    private readonly List<Book> _books = new();
+    private readonly List<Book> _books = [];
 
     public Name Name { get; set; }
     public IEnumerable<Book> Books => _books.AsReadOnly();
--- src/Backend/Library.Infrastructure/DTO/PublisherDto.cs
+++ src/Backend/Library.Infrastructure/DTO/PublisherDto.cs
@@ -5,5 +5,5 @@ namespace Library.Infrastructure.DTO;
 public record PublisherDto
 {
     public Guid Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
 }
