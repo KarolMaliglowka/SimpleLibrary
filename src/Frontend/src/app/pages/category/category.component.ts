e4c37520a419ff6918d89faaf9f83857532fb921
Karol <karol_maliglowka@O2.pl>
2025-09-03 20:56:31 +0200

    added part of categories list

--- src/Frontend/src/app/pages/category/category.component.ts
+++ src/Frontend/src/app/pages/category/category.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {ToolbarModule} from 'primeng/toolbar';
+import {TableModule} from 'primeng/table';
+import {IconFieldModule} from 'primeng/iconfield';
+import {InputIconModule} from 'primeng/inputicon';
+import {InputTextModule} from 'primeng/inputtext';
+
+import {Category} from './category';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [ButtonModule, ToolbarModule, TableModule, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent {
 
+    categories!: Category[];
+    category!: Category;
+
+    openNew() {
+
+    }
+
+    editCategory(category: Category) {}
+
+    deleteCategory(category: Category) {}
 }
