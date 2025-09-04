bef34da14054b237cd4b063c062f8edaab636484
Karol <karol_maliglowka@O2.pl>
2025-09-04 16:15:29 +0200

    changes

--- src/Frontend/src/app/pages/category/category.component.ts
+++ src/Frontend/src/app/pages/category/category.component.ts
@@ -1,23 +1,48 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {ToolbarModule} from 'primeng/toolbar';
-import {TableModule} from 'primeng/table';
+import {Table, TableModule} from 'primeng/table';
 import {IconFieldModule} from 'primeng/iconfield';
 import {InputIconModule} from 'primeng/inputicon';
 import {InputTextModule} from 'primeng/inputtext';
-
+import {CategoriesService} from '../service/category.service';
 import {Category} from './category';
+import {ConfirmationService, MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-category',
   imports: [ButtonModule, ToolbarModule, TableModule, IconFieldModule, InputIconModule, InputTextModule],
+    providers: [MessageService, ConfirmationService, CategoriesService],
   templateUrl: './category.component.html',
+    standalone: true,
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
 
     categories!: Category[];
     category!: Category;
+    @ViewChild('dt') dt!: Table;
+
+    constructor(
+        private categoriesService: CategoriesService,
+        private cd: ChangeDetectorRef
+    ) {
+    }
+
+
+    ngOnInit() {
+        this.loadData();
+    }
+
+    loadData() {
+        this.categoriesService.GetAllCategories()
+            .then((data) => {
+                console.log(data);
+                this.categories = data;
+                this.cd.markForCheck();
+            }).catch(() => {
+        });
+    }
 
     openNew() {
 
@@ -26,4 +51,7 @@ export class CategoryComponent {
     editCategory(category: Category) {}
 
     deleteCategory(category: Category) {}
+
+    protected readonly name = name;
 }
+
